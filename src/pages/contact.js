efbfe0ea6c5ac0b39a3d3d9cafcab3d235bcd2cb
alfficcadenti <alfredoficcadenti@gmail.com>
2019-04-27 21:31:48 +0100

    fixed warning for unused component

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const ContactPage = () => (
   <Layout>
     <SEO title="Contact" />
-    <div>{'I love to be in touch with anyone who visits the website. Give me a buzz and drop me a line to '}
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      {'I love to be in touch with anyone who visits the website. Give me a buzz and drop me a line to '}
       <a href="mailto: {'info@londonnectar.com'}">{'info@londonnectar.com'}</a>
     </div>
   </Layout>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-// import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-    </div>
-    <Link to="/contact/">Contact</Link>
-
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
+    
   </Layout>
 )
 
