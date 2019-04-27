95a9564136b8f6b6b6d09750443b2bfd2ab21d80
alfficcadenti <alfredoficcadenti@gmail.com>
2019-04-27 22:05:10 +0100

    test import styles css

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import 'popper.js'
-import './bootstrap.css';
+import styles from './bootstrap.css';
 
 const Header = ({ siteTitle, siteDescription, menuLinks }) => (
   <header id='header'
