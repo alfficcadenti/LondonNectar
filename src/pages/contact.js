748b17195b1d51b523bb466048f77d453a379d94
Alfredo Ficcadenti <aficcadenti@homeaway.com>
2021-03-23 10:38:05 +0000

    fix link to email address

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,7 @@ const ContactPage = () => (
     <SEO title="Contact" keywords={[`honey`, `raw honey`, `local honey`, `london`, `london honey`, `beekeeping`, `beekeeper`]}/>
     <div style={{ maxWidth: `100%`, marginBottom: `1.45rem` }}>
       {'I love to be in touch with anyone who visits the website. Give me a buzz and drop me a line to '}
-      <a href="mailto: {'info@londonnectar.com'}">{'info@londonnectar.com'}</a>
+      <a href="mailto: info@londonnectar.com">{'info@londonnectar.com'}</a>
     </div>
   </Layout>
 )
